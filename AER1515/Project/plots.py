6c0d5437cfb8579885c0f23a6c9bdcf794af12d1
alexis-bruneau <alexis1999bruneau@gmail.com>
2022-12-07 16:27:14 -0500

    update plot

--- AER1515/Project/plots.py
+++ AER1515/Project/plots.py
@@ -54,18 +54,52 @@ def butter_lowpass_filtfilt(data, cutoff=1500, fs=50000, order=5):
     return filtfilt(b, a, data)  # forward-backward filter
 
 
-def plot_one_box(x, img, color=None, label=None, line_thickness=3):
+def plot_one_box(x, img,point_list,point_list2,color=None, label=None, line_thickness=3):
     # Plots one bounding box on image img
     tl = line_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
     color = color or [random.randint(0, 255) for _ in range(3)]
     c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
+    cx = int((int(x[0])+int(x[2]))/2)
+    cy = int((int(x[1])+int(x[3]))/2)
     cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
+    
+    # Calculate threshold 
+    min_y_threshold = (min(point_list, key=lambda tup: tup[1]))[1]
+    max_y_threshold = (max(point_list, key=lambda tup: tup[1]))[1]
+    min_x_threshold = (min(point_list, key=lambda tup: tup[0]))[0]
+    max_x_threshold = (max(point_list, key=lambda tup: tup[0]))[0]
+   
+
+    min_y1_threshold = (min(point_list2, key=lambda tup: tup[1]))[1]
+    max_y1_threshold = (max(point_list2, key=lambda tup: tup[1]))[1]
+    min_x1_threshold = (min(point_list2, key=lambda tup: tup[0]))[0]
+    max_x1_threshold = (max(point_list2, key=lambda tup: tup[0]))[0]
+
     if label:
+        # Calculate position of person
+        if (cx > min_x_threshold and cx < max_x_threshold) and (cy > min_y_threshold and cy < max_y_threshold):
+            cv2.circle(img, (cx,cy) , radius=10, color =[0,255,0], thickness=-1) #plot center of bounding box
+            inbound = True
+            region = "R1"
+
+        elif (cx > min_x1_threshold and cx < max_x1_threshold) and (cy > min_y1_threshold and cy < max_y1_threshold):
+            cv2.circle(img, (cx,cy) , radius=10, color =[255,0,0], thickness=-1) #plot center of bounding box
+            inbound = True
+            region = "R2"
+
+        else:
+            cv2.circle(img, (cx,cy) , radius=10, color =[0,0,255], thickness=-1) #plot center of bounding box
+            region = None
+            inbound = False
+       
+
         tf = max(tl - 1, 1)  # font thickness
         t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
         c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
         cv2.rectangle(img, c1, c2, color, -1, cv2.LINE_AA)  # filled
         cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+        
+    return cx, cy, inbound, region
 
 
 def plot_one_box_PIL(box, img, color=None, label=None, line_thickness=None):
